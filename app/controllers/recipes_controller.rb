ce50c9e320d23297f70320ffc218f354873553c1
ikumi-taka <canomori0311@gmail.com>
2023-03-14 11:49:14 +0900

    recipe投稿登録できるようになった

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,18 @@ class RecipesController < ApplicationController
   def new       # レシピの新規投稿
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save!
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:recipe_name, :genre, :food, :quantity, :content, user_ids: [])
+    end
 end
