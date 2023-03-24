9f3ef2a97ca7d9ea41d6acce349fb5860b0daf62
ikumi-taka <canomori0311@gmail.com>
2023-03-24 21:06:33 +0900

    投稿したレシピの一覧表示機能実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,15 @@
 class RecipesController < ApplicationController
+  def index
+    @recipe = Recipe.all
+  end
+
   def new       # レシピの新規投稿
     @recipe = Recipe.new
   end
 
   def create
     @recipe = Recipe.new(recipe_params)
-    if @recipe.save!
+    if @recipe.save
       redirect_to root_path
     else
       render :new
@@ -14,6 +18,6 @@ class RecipesController < ApplicationController
 
   private
     def recipe_params
-      params.require(:recipe).permit(:recipe_name, :genre, :food, :quantity, :content, user_ids: [])
+      params.require(:recipe).permit(:recipe_name, :genre, :food, :quantity, :content).merge(user_id: current_user.id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users # ユーザー編集画面は作成しない（予定）
   root 'homes#index'
   resources :recipes
+  resources :users
 end
